be8c0f181cfc9455213281a9d51bbfefde7e3675
micuat <micuat@gmail.com>
2022-11-25 23:34:22 +0100

    airtable + chevereto

--- backend/twitter-gallery.js
+++ backend/twitter-gallery.js
@@ -2,6 +2,12 @@ const multer = require('multer')
 var path = require('path')
 var fs = require('fs')
 const request = require('request')
+const superagent = require('superagent')
+const Airtable = require("airtable");
+
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+  process.env.AIRTABLE_BASE
+);
 
 var tweet
 module.exports = (app) => {
@@ -92,36 +98,125 @@ module.exports = (app) => {
     })
 
   //  console.log('envv', process.env)
-    if(process.env.CONSUMER_KEY) {
      const upload = multer({ storage: storage });
      app.post("/image", upload.single('previewImage'), (req, res) => {
-       const updatedNameString = req.query.name.replace('@', '{@}')
 
-       findParentTweet(req.query.sketch_id, function(err, tweet_id){
-         if(err) console.log(err)
-        //  if(tweet_id !== null) console.log("FOUND PARENT", tweet_id)
+      if(process.env.CHEVERETO_API_KEY && process.env.CHEVERETO_API_URL) {
+        console.log('UPLOADING TO CHEVERETO');
+        superagent
+          .post(process.env.CHEVERETO_API_URL)
+          .field('key', process.env.CHEVERETO_API_KEY)
+          .attach('source', req.file.path, {filename: `${req.query.name}: ${req.query.sketch_id}`})
+          .end((err, res) => {
+            if (err) {
+              console.log(err)
+            } else {
+              console.log('Media uploaded!')
+              // console.log(res.body.image)
 
-        /* uncomment below to upload to twitter */
-        tweet.post_chunked({
-           imagePath: req.file.path,
-           url: req.query.url,
-           name: updatedNameString,
-           parent_tweet: tweet_id
-         }, function(err, data){
-           if(err){
-             console.log('ERROR POSTING IMAGE', err)
-           } else {
-            //  console.log('tweet id is ', data.id_str)
-             res.status(200).send( 'https://twitter.com/hydra_patterns/status/' + data.id_str );
-             db.update(
-               { _id: req.query.sketch_id },
-               { $set: { tweet_id: data.id_str,  bitly_hash: data.bitly_hash }
-             }, function (err, numReplaced) {});
-           }
-         })
-       
-       })
-    
+              if(process.env.NOCODB_API_KEY && process.env.NOCODB_API_URL) {
+                db.find({_id: req.query.sketch_id}, function (err, entries){
+                  if (err) {
+                    console.log('problem with db', err)
+                  } else {
+                    if (entries.length > 0) {
+                      const code = decodeURIComponent(Buffer.from(entries[0].code, 'base64').toString('binary'));
+                      const image = JSON.stringify([
+                        {
+                          url: res.body.image.url,
+                          title: res.body.image.filename,
+                          mimetype: res.body.image.mime,
+                          size: res.body.image.size,
+                        }
+                      ])
+                      superagent
+                      .post(process.env.NOCODB_API_URL)
+                      .set('xc-auth', process.env.NOCODB_API_KEY)
+                      .send({
+                        title: `${req.query.name}`,
+                        image,
+                        url: `https://hydra.glitches.me/?sketch_id=${req.query.sketch_id}`,
+                        code,
+                      })
+                      .end((err, res) => {
+                        if (err) {
+                          console.log(err)
+                        } else {
+                          console.log('NOCODB uploaded!')
+                        }
+                      })
+                    }
+                  }
+                })
+              }
+              if (base) {
+                base('Table 1').create([
+                  {
+                    "fields": {
+                      "Name": `${req.query.name}`,
+                      "Notes": `https://hydra.glitches.me/?sketch_id=${req.query.sketch_id}`,
+                      "Attachments": [
+                        {
+                          "url": res.body.image.url
+                        }
+                      ],
+                      "tag": [
+                        "sketch"
+                      ],
+                      "featured": false,
+                    }
+                  },
+                ], function(err, records) {
+                  if (err) {
+                    console.error(err);
+                    return;
+                  }
+                  records.forEach(function (record) {
+                    console.log(record.getId());
+                  });
+                });
+              }
+            }
+          });
+      }
+
+      if(process.env.CONSUMER_KEY) {
+      //  console.log('UPLOADING');
+      //  findParentTweet(req.query.sketch_id, function(err, tweet_id){
+      //    if(err) console.log(err)
+      //    console.log('posting image');
+      //    if(tweet_id !== null) console.log("FOUND PARENT", tweet_id)
+      //   tweet.post_chunked({
+      //      imagePath: req.file.path,
+      //      url: req.query.url,
+      //      name: req.query.name,
+      //      parent_tweet: tweet_id
+      //    }, function(err, data){
+      //      if(err){
+      //        console.log('ERROR POSTING IMAGE', err)
+      //      } else {
+      //        console.log('tweet id is ', data.id_str)
+      //        res.status(200).send( 'https://twitter.com/hydra_patterns/status/' + data.id_str );
+      //        db.update(
+      //          { _id: req.query.sketch_id },
+      //          { $set: { tweet_id: data.id_str,  bitly_hash: data.bitly_hash }
+      //        }, function (err, numReplaced) {});
+      //      }
+      //    })
+      //  })
+       // find out whether sketch has a parent, and if the parent has a corresponding tweet
+
+      // console.log('FOUND TWEET', tweet_id)
+       // tweet.post_image('testing', req.file.buffer, function (err) {
+       //   console.log('UPLOADED', err)
+       // })
+      // saveFile(req.file, "test.png")
+      //
+      //  req.query.url
+
+
+      // res.end();
+      }
      });
 
     function findParentTweet(sketch_id, callback) {
@@ -154,7 +249,6 @@ module.exports = (app) => {
          }
        }
      })
-    }
     }
 
      function saveFile(body, fileName) {
